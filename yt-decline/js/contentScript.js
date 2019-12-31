2fcbcea88a9b54d3a1f31562eaf3f67830053135
Anders Steen <anders.steen98@gmail.com>
2019-12-31 13:33:52 +0100

    fixed issue #2 subscriptions disappearing

--- yt-decline/js/contentScript.js
+++ yt-decline/js/contentScript.js
@@ -9,51 +9,60 @@ var ignoreURLStrings = ["subscriptions",
                         "playlist",
                         "library"]
 
+console.log("Started declining recommended videos every second.")
+
+// Repeat decline every second.
+setInterval(declineVideos, 1000)
+
+
 function declineVideos() {
+  // If the URL should not be ignored, hide the videos.
   if (!ignoreURL()) {
-    // Hide by tag name.
-    for (tag of hideTags) {
-      var elements = document.getElementsByTagName(tag)
-      for (element of elements) {
-        if (element != null) {
-          element.style.display = "none"
-        }
-      }
-    }
-
-    // Hide by id.
-    for (id of hideIds) {
-      var element = document.getElementById(id)
-
-      if (element != null) {
-        element.style.display = "none"
-      }
-    }
-
-    // Hide by class name.
-    for (name of hideClassnames) {
-      var elements = document.getElementsByClassName(name)
-      for (element of elements) {
-        if (element != null) {
-          element.style.display = "none"
-        }
-      }
-    }
+    operateOnVideos(function(element) {element.style.visibility = "hidden"})
+  } else { // Otherwise, show the videos.
+    operateOnVideos(function(element) {element.style.visibility = "visible"})
   }
 }
 
 function ignoreURL() {
   var ignore = false
+  var href = window.location.href
 
   for (string of ignoreURLStrings) {
-    if (window.location.href.indexOf(string) > -1) {
+    if (href.indexOf(string) > -1) {
       ignore = true
     }
   }
   return ignore
 }
 
-console.log("Started declining recommended videos every second.")
+function operateOnVideos(operation) {
+  // Operate by tag name.
+  for (tag of hideTags) {
+    var elements = document.getElementsByTagName(tag)
+    for (element of elements) {
+      if (element != null) {
+        operation(element)
+      }
+    }
+  }
 
-// Repeat decline every second.
-setInterval(declineVideos, 1000)
+  // Operate by id.
+  for (id of hideIds) {
+    var element = document.getElementById(id)
+
+    if (element != null) {
+      operation(element)
+    }
+  }
+
+  // Operate by class name.
+  for (name of hideClassnames) {
+    var elements = document.getElementsByClassName(name)
+    for (element of elements) {
+      if (element != null) {
+        operation(element)
+      }
+    }
+  }
+}
