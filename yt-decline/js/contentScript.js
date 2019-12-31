af4862a1a2701944a8fbc80d0b6bbf1118237585
Anders Steen <anders.steen98@gmail.com>
2019-12-31 13:41:18 +0100

    fix issue #1 with endcard videos not being declined

--- yt-decline/js/contentScript.js
+++ yt-decline/js/contentScript.js
@@ -2,7 +2,11 @@ console.log("Youtube Decline running.")
 
 var hideTags =         ["ytd-two-column-browse-results-renderer"]
 var hideIds =          ["related"]
-var hideClassnames =   ["html5-endscreen ytp-player-content videowall-endscreen ytp-endscreen-paginate ytp-show-tiles"]
+var hideClassnames =   ["html5-endscreen ytp-player-content videowall-endscreen ytp-endscreen-paginate ytp-show-tiles",
+                        "ytp-ce-element ytp-ce-video ytp-ce-top-left-quad ytp-ce-size-640 ytp-ce-element-show",
+                        "ytp-ce-element ytp-ce-video ytp-ce-top-right-quad ytp-ce-size-640 ytp-ce-element-show",
+                        "ytp-ce-element ytp-ce-video ytp-ce-bottom-left-quad ytp-ce-size-640 ytp-ce-element-show",
+                        "ytp-ce-element ytp-ce-video ytp-ce-bottom-right-quad ytp-ce-size-640 ytp-ce-element-show"]
 
 var ignoreURLStrings = ["subscriptions",
                         "history",
@@ -14,7 +18,7 @@ console.log("Started declining recommended videos every second.")
 // Repeat decline every second.
 setInterval(declineVideos, 1000)
 
-
+// Hides videos.
 function declineVideos() {
   // If the URL should not be ignored, hide the videos.
   if (!ignoreURL()) {
@@ -24,6 +28,7 @@ function declineVideos() {
   }
 }
 
+// Returns true if videos should not be hidden on the site.
 function ignoreURL() {
   var ignore = false
   var href = window.location.href
@@ -36,6 +41,8 @@ function ignoreURL() {
   return ignore
 }
 
+// Performs the given operation on all elements that are covered by the arrays.
+// The operation parameter must be a function that takes a single argument.
 function operateOnVideos(operation) {
   // Operate by tag name.
   for (tag of hideTags) {
