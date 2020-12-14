b11e9f9d4ce002ecf8d334da0c3ee53a13c8d31d
Anders Steen <anders.steen98@gmail.com>
2020-12-14 15:56:28 +0100

    add extension toggle button, issue #5

---
+++ yt-decliner/js/button.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    var button = document.getElementById("button");
+
+    button.onclick = function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+            let regex = RegExp(".*:\/\/.*\.youtube\.com.*");
+            if (!regex.test(tabs[0].url)) {
+                console.log("YouTube not selected.");
+                return;
+            }
+
+            chrome.tabs.sendMessage(tabs[0].id, { action: "toggle" }, function (response) {
+                if (response.enabled) {
+                    chrome.browserAction.setIcon({ path: "/images/icon_128.png" });
+                } else {
+                    chrome.browserAction.setIcon({ path: "/images/icon_128_off.png" });
+                }
+            });
+        });
+    }
+}
--- yt-decliner/js/contentScript.js
+++ yt-decliner/js/contentScript.js
@@ -1,20 +1,44 @@
 console.log("Youtube Decliner running.")
 
-var hideTags =         ["ytd-two-column-browse-results-renderer"]
-var hideIds =          ["related"]
-var hideClassnames =   ["ytp-ce-element",
-                        "ytp-endscreen-content"]
+var hideTags = ["ytd-two-column-browse-results-renderer"]
+var hideIds = ["related"]
+var hideClassnames =
+  ["ytp-ce-element",
+    "ytp-endscreen-content"]
 
-var ignoreURLStrings = ["subscriptions",
-                        "history",
-                        "playlist",
-                        "library",
-                        "user",
-                        "channel",
-                        "my_videos",
-                        "/c/"]
+var ignoreURLStrings =
+  ["subscriptions",
+    "history",
+    "playlist",
+    "library",
+    "user",
+    "channel",
+    "my_videos",
+    "/c/"]
 
-console.log("Started declining recommended videos every half second.")
+// Add listener to listen for extension toggling.
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  if (msg.action === "toggle") {
+    // Get the enabled status from storage, default to true.
+    chrome.storage.local.get({enabled: true}, function(data) {
+      // Store the opposite of the current enabled status in storage.
+      chrome.storage.local.set({ enabled: !data.enabled }, function() {
+        printEnabledMessage(!data.enabled);
+        sendResponse({enabled: !data.enabled});
+      });
+    });
+  }
+  return true;
+});
+
+// Prints the current enabled status to the console.
+function printEnabledMessage(enabled) {
+  if (enabled) {
+    console.log("Started declining recommended videos every half second.")
+  } else {
+    console.log("Stopped declining videos.")
+  }
+}
 
 // Repeat decline every second.
 setInterval(declineVideos, 500)
@@ -41,40 +65,46 @@ function contains(string, substring) {
 // Performs the given operation on all elements that are covered by the arrays.
 // The operation parameter must be a function that takes a single argument.
 function declineVideos() {
-  // Operate by tag name.
-  for (tag of hideTags) {
-    var elements = document.getElementsByTagName(tag)
-    for (element of elements) {
-      if (element != null) {
-        // Unhide tags in ignored URLs
-        if (ignoreURL()) {
-          element.style.visibility = "visible"
-        } else {
-          element.style.visibility = "hidden"
+  chrome.storage.local.get({enabled: true}, function (data) {
+    if (!data.enabled) {
+      return;
+    }
+
+    // Operate by tag name.
+    for (tag of hideTags) {
+      var elements = document.getElementsByTagName(tag)
+      for (element of elements) {
+        if (element != null) {
+          // Unhide tags in ignored URLs
+          if (ignoreURL()) {
+            element.style.visibility = "visible"
+          } else {
+            element.style.visibility = "hidden"
+          }
         }
       }
     }
-  }
 
-  // Operate by id.
-  for (id of hideIds) {
-    var element = document.getElementById(id)
+    // Operate by id.
+    for (id of hideIds) {
+      var element = document.getElementById(id)
 
-    if (element != null) {
-      element.style.visibility = "hidden"
-    }
-  }
-
-  // Operate by class name on divs.
-  for (name of hideClassnames) {
-    var allDivs = document.getElementsByTagName('div');
-
-    for (var i = 0; i < allDivs.length; i++) {
-      var div = allDivs[i];
-
-      if (contains(div.className, name)) {
-        div.style.visibility = "hidden"
+      if (element != null) {
+        element.style.visibility = "hidden"
       }
     }
-  }
+
+    // Operate by class name on divs.
+    for (name of hideClassnames) {
+      var allDivs = document.getElementsByTagName('div');
+
+      for (var i = 0; i < allDivs.length; i++) {
+        var div = allDivs[i];
+
+        if (contains(div.className, name)) {
+          div.style.visibility = "hidden"
+        }
+      }
+    }
+  });
 }
